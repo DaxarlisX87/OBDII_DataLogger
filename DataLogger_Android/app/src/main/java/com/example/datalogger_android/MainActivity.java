b9d9fdac8d0c3a27c1475f9d386146649e6a7472
Daxarlis <DaxarlisX87@gmail.com>
2020-02-23 15:08:03 -0500

    Added Bluetooth Commands to Main

--- DataLogger_Android/app/src/main/java/com/example/datalogger_android/MainActivity.java
+++ DataLogger_Android/app/src/main/java/com/example/datalogger_android/MainActivity.java
@@ -12,8 +12,54 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+// For Bluetooth Connectivity
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice = null;
+
+    final byte delimiter = 33;
+    int readBufferPosition = 0;
+
+
+    public void sendBtMsg(String msg2send){
+        //UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+        UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"); //Standard SerialPortService ID
+        try {
+
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            if (!mmSocket.isConnected()){
+                mmSocket.connect();
+            }
+
+            String msg = msg2send;
+            //msg += "\n";
+            OutputStream mmOutputStream = mmSocket.getOutputStream();
+            mmOutputStream.write(msg.getBytes());
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +68,133 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Handler handler = new Handler();
+        final TextView myLabel = (TextView) findViewById(R.id.btResult);
+
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        Button download = findViewById(R.id.download_button);
+
+        final class workerThread implements Runnable {
+
+            private String btMsg;
+
+            public workerThread(String msg) {
+                btMsg = msg;
+            }
+
+            public void run()
+            {
+                sendBtMsg(btMsg);
+                while(!Thread.currentThread().isInterrupted())
+                {
+                    int bytesAvailable;
+                    boolean workDone = false;
+
+                    try {
+
+
+
+                        final InputStream mmInputStream;
+                        mmInputStream = mmSocket.getInputStream();
+                        bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            Log.e("Aquarium recv bt","bytes available" + bytesAvailable);
+                            byte[] readBuffer = new byte[1024];
+                            mmInputStream.read(packetBytes);
+
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    Log.e("Aquarium recv bt","delimiter reached at:" + i);
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    //The variable data now contains our full command
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            myLabel.setText(data);
+                                        }
+                                    });
+
+                                    workDone = true;
+                                    break;
+
+
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+
+                            if (workDone == true){
+                                mmSocket.close();
+                                break;
+                            }
+
+                        }
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+
+        download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToFileDownload();
             }
         });
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    (new Thread(new workerThread("start"))).start();
+                } else {
+                    // The toggle is disabled
+                    (new Thread(new workerThread("stop"))).start();
+
+                }
+            }
+        });
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("DataLoggerPi")) //Note, you will need to change this to match the name of your device
+                {
+                    Log.e("Aquarium",device.getName());
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+
+
     }
 
     @Override
@@ -58,4 +224,7 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(intent);
     }
+
+
+
 }
